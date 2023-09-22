20e3e76a2e2c0a481235ba528a34d6bcd1048a92
Mahjabin Mim <mahjabinmim711@gmail.com>
2023-09-22 23:23:53 +0600

    Add files via upload

---
+++ Pages/Projects.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace portfolio.Pages;
+
+public class ProjectsModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public ProjectsModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
+
---
+++ Pages/References.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace portfolio.Pages;
+
+public class ReferencesModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public ReferencesModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
+
